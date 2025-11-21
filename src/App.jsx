0e55c638498fab4c6a59769e1114fdad754ddb00
MAICOL ESNEIDER <kingofthieves465@gmail.com>
2025-11-21 00:47:16 -0500

    refactor: extracción de datos personales en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import StackTecnologias from "./components/StackTecnologias";
 import CabeceraCV from "./components/CabeceraCV";
 import Perfil from "./components/Perfil";
 import Experiencia from "./components/Experiencia";
 import Educacion from "./components/Educacion";
+import StackTecnologias from "./components/StackTecnologias";
+
+import {
+  datosPersonales,
+  perfilProfesional,
+  experiencias,
+  estudios,
+  tecnologias
+} from "./cvData";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="contenedor-cv">
-      <CabeceraCV />
-      <Perfil />
-      <Experiencia />
-      <Educacion />
-      <StackTecnologias />
+      <CabeceraCV
+        nombre={datosPersonales.nombre}
+        cargo={datosPersonales.cargo}
+        ciudad={datosPersonales.ciudad}
+        contacto={datosPersonales.contacto}
+      />
+
+      <Perfil resumen={perfilProfesional} />
+
+      <Experiencia lista={experiencias} />
+
+      <Educacion lista={estudios} />
+
+      <StackTecnologias lista={tecnologias} />
     </div>
   );
 }
 
 export default App;
+
+      
---
+++ src/cvData.jsx
@@ -0,0 +1,31 @@
+export const datosPersonales = {
+  nombre: "Maicol Esneider Posada",
+  cargo: "Desarrollador Frontend",
+  ciudad: "Medellín",
+  contacto: "maicol@example.com"
+};
+
+export const perfilProfesional =
+  "Soy un aprendiz en desarrollo, con conocimientos en React, lógica y construcción de interfaces.";
+
+export const experiencias = [
+  { cargo: "Frontend Jr", empresa: "TechNova", periodo: "2022 - 2023" },
+  { cargo: "Soporte TI", empresa: "SoftHelp", periodo: "2021" },
+  { cargo: "Freelancer", empresa: "Proyectos Personales", periodo: "2020 - 2022" }
+];
+
+export const estudios = [
+  { institucion: "SENA", curso: "ADSO", año: 2024 },
+  { institucion: "Google", curso: "Fundamentos Web", año: 2023 },
+  { institucion: "Platzi", curso: "React Básico", año: 2023 }
+];
+
+export const tecnologias = [
+  "JavaScript",
+  "React",
+  "HTML",
+  "CSS",
+  "Node.js",
+  "Git",
+  "GitHub"
+];
