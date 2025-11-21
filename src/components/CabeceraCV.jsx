9ee2c0b5ee5e364b685feac81db015cd85997ce7
MAICOL ESNEIDER <kingofthieves465@gmail.com>
2025-11-21 01:29:21 -0500

    feat: componente CabeceraCV ahora recibe props

--- src/components/CabeceraCV.jsx
+++ src/components/CabeceraCV.jsx
@@ -1,13 +1,12 @@
-function CabeceraCV() {
+function CabeceraCV({ nombre, cargo, ciudad, contacto }) {
   return (
     <header>
-      <h1>Maicol Esneider Posada Callejas</h1>
-      <h2>Desarrollador Front-End Jr.</h2>
+      <h1>{nombre}</h1>
+      <h2>{cargo}</h2>
 
       <ul>
-        <li>📍 Medellín, Colombia</li>
-        <li>📞 3244534659</li>
-        <li>📧 kingofthieves465@gmail.com</li>
+        <li>📍 {ciudad}</li>
+        <li>📞 {contacto}</li>
       </ul>
     </header>
   );
